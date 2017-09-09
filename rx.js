523181fc332d24df63aa76640748909017a5370c
MaksymI <maksym.izmailov.lv@gmail.com>
2017-09-09 20:12:00 +0300

    next

--- rx.js
+++ rx.js
@@ -1,42 +1,45 @@
 'use strict'
+// ------------------------------------------------
+function observe(observable, next, done) {
+    for (let letter of observable) {
+        next(letter);
+    }
+    done();
+}
 
-// function observe(observable, next, done) {
-//     for (let letter of observable) {
-//         next(letter);
-//     }
-//     done();
-// }
+observe('Observable', letter => console.log(letter), () => console.log('done'));
 
-// observe('Observable', letter => console.log(letter), () => console.log('done'));
+// ----------------------------------------------------
 
-// class Observable {
-//     constructor(source) {
-//         this.source = source.split('');
-//         this.result = this.source;
-//     }
+class Observable {
+    constructor(source) {
+        this.source = source.split('');
+        this.result = this.source;
+    }
 
-//     subscribe(next) {
-//         for (let item of this.result) {
-//             next(item);
-//         }
-//     }
+    subscribe(next) {
+        for (let item of this.result) {
+            next(item);
+        }
+    }
 
-//     filter(predicate) {
-//         this.result = this.result.filter(predicate);
-//         return this;
-//     }
+    filter(predicate) {
+        this.result = this.result.filter(predicate);
+        return this;
+    }
 
-//     map(callback) {
-//         this.result = this.result.map(callback);
-//         return this;
-//     }
-// }
+    map(callback) {
+        this.result = this.result.map(callback);
+        return this;
+    }
+}
 
-// new Observable('Observable')
-//     .map(letter => letter.toUpperCase())
-//     .filter(letter => letter === 'O')
-//     .subscribe(letter => console.log(letter));
+new Observable('Observable')
+    .map(letter => letter.toUpperCase())
+    .filter(letter => letter === 'O')
+    .subscribe(letter => console.log(letter));
 
+// -------------------------------------------------------
 
 const Rx = require('rx');
 
